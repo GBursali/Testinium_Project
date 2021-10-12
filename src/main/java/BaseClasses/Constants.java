185aad8b9e5ce70cea3308b80af48aa4e8b26b45
GBursali <gurkanbursali@hotmail.com>
2021-10-12 19:22:05 +0300

    initial commit

--- src/main/java/BaseClasses/Constants.java
+++ src/main/java/BaseClasses/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String BASEURL = "https://www.n11.com/";
     public static final String USERNAME = "bursaligu@gmail.com";
     public static final String PASSWORD = "tADhkRmtj9ULGUK";
+    public static final String SEARCH_KEYWORD = "bilgisayar";
 }
