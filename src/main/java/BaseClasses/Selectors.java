a9bd271f08db79314b793f31cad22d148ebc9df0
GBursali <gurkanbursali@hotmail.com>
2021-10-13 12:21:44 +0300

    Fix Typo

--- src/main/java/BaseClasses/Selectors.java
+++ src/main/java/BaseClasses/Selectors.java
@@ -6,7 +6,7 @@ public class Selectors {
     public static By LOGIN_USERNAME = By.id("email");
     public static By LOGIN_PASSWORD = By.id("password");
     public static By LOGIN_SUBMIT = By.id("loginButton");
-    public static By LOGIN_INFO = By.cssSelector(".menulink.user");
+    public static By LOGIN_INFO = By.cssSelector(".menuLink.user");
 
     public static By SEARCH_INPUT = By.id("searchData");
     public static By SEARCH_SUBMIT = By.cssSelector(".searchBtn");
